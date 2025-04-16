2885d17809ca1953f32cbffd90c06d81c5fb7ae9
Mccullahz <zylardmccullah@gmail.com>
2025-04-16 14:16:54 -0400

    main.js setup to be functional~ish. can click button to output algos

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,43 +1,35 @@
+{/*I AM NOT A JS KING ------- UP TO YOU GUYS TO MAKE THIS SUCKER PRETTY */}
 import './style.css';
 import './app.css';
 
 import logo from './assets/images/logo-universal.png';
-import {Greet} from '../wailsjs/go/main/App';
+import { FCFS, RR } from '../wailsjs/go/main/App';
 
 document.querySelector('#app').innerHTML = `
-    <img id="logo" class="logo">
-      <div class="result" id="result">Please enter your name below 👇</div>
-      <div class="input-box" id="input">
-        <input class="input" id="name" type="text" autocomplete="off" />
-        <button class="btn" onclick="greet()">Greet</button>
-      </div>
-    </div>
+  <img id="logo" class="logo">
+  <div class="title">Process Scheduling Simulator</div>
+  <div class="button-group">
+    <button class="btn" onclick="runFCFS()">Run FCFS</button>
+    <button class="btn" onclick="runRR()">Run Round Robin</button>
+  </div>
+  <pre class="result" id="result">← Select an algorithm to begin</pre>
 `;
 document.getElementById('logo').src = logo;
 
-let nameElement = document.getElementById("name");
-nameElement.focus();
 let resultElement = document.getElementById("result");
 
-// Setup the greet function
-window.greet = function () {
-    // Get name
-    let name = nameElement.value;
 
-    // Check if the input is empty
-    if (name === "") return;
 
-    // Call App.Greet(name)
-    try {
-        Greet(name)
-            .then((result) => {
-                // Update result with data back from App.Greet()
-                resultElement.innerText = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
-    }
+{/* VERY VERY ROUGH HOW TO OUTPUT ALGOS IN WINDOW, CAN CHANGE AS MUCH AS NEEDED*/}
+window.runFCFS = function () {
+    FCFS().then((result) => {
+        resultElement.innerText = result;
+    }).catch(console.error);
 };
+
+window.runRR = function () {
+    RR().then((result) => {
+        resultElement.innerText = result;
+    }).catch(console.error);
+};
+
